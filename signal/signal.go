abe4da50dc10805cabfb92da23a5f6d2582a19e7
程飞 <fay.cheng.cn@gmail.com>
2019-09-29 15:57:30 +0800

    refactor signal: 1, remove sender; 2, remove namespace

--- signal/signal.go
+++ signal/signal.go
@@ -5,63 +5,33 @@ import (
 	"sync"
 )
 
-const (
-	ALL = "ALL"
-)
-
 type Receiver func(c context.Context, args ...interface{}) error
 
 type Signal struct {
-	Receivers  sync.Map
+	sync.RWMutex
+	Receivers  []Receiver
 	StrictMode bool
 }
 
-func NewSignal(strictMode bool) *Signal {
+func NewSignal() *Signal {
 	return &Signal{
-		StrictMode: strictMode,
+		Receivers: make([]Receiver, 0),
 	}
 }
 
-func (s *Signal) Connect(receiver Receiver, sender string) error {
-	var receivers []Receiver
-	if sender == "" {
-		sender = ALL
-	}
-	receiversIface, ok := s.Receivers.Load(sender)
-	if !ok {
-		receivers = make([]Receiver, 0)
-	} else {
-		receivers = receiversIface.([]Receiver)
-	}
-	receivers = append(receivers, receiver)
-	s.Receivers.Store(sender, receivers)
+func (s *Signal) Connect(receiver Receiver) error {
+	s.Receivers = append(s.Receivers, receiver)
 	return nil
 }
 
-func (s *Signal) SendTo(c context.Context, sender string, args ...interface{}) error {
-	receiversIface, ok := s.Receivers.Load(sender)
-	if !ok {
-		return nil
-	}
-	receivers := receiversIface.([]Receiver)
-	for _, receiver := range receivers {
+func (s *Signal) Send(c context.Context, args ...interface{}) error {
+	s.Lock()
+	defer s.Unlock()
+	for _, receiver := range s.Receivers {
 		err := receiver(c, args...)
-		if err != nil && s.StrictMode {
+		if err != nil {
 			return err
 		}
 	}
 	return nil
-}
-
-func (s *Signal) Send(c context.Context, sender string, args ...interface{}) error {
-	var err error
-	err = s.SendTo(c, ALL, args...)
-	if err != nil && s.StrictMode {
-		return err
-	}
-	if sender == "" || sender == ALL {
-		return nil
-	}
-	err = s.SendTo(c, sender, args...)
-	return err
 }
